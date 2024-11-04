84ea6da8047417894c63644fba19f02dbfa0a04e
MrRapaz <ghov2222@gmail.com>
2024-11-04 10:28:26 -0300

    Adicionando Frontend

--- dw3backend/apps/login/controller/ctlLogin.js
+++ dw3backend/apps/login/controller/ctlLogin.js
@@ -3,29 +3,30 @@ const bCrypt = require("bcryptjs");
 const mdlLogin = require("../model/mdlLogin");
 
 const Login = async (req, res, next) => { 
-
-  const credencial = await mdlLogin.GetCredencial(req.body.username);  
-  if (credencial.length == 0) {
-    return res.status(401).json({ message: "Usuário não identificado!" });    
-  }
   
-  if (bCrypt.compareSync(req.body.password, credencial[0].password)) {
+  const credencial = await mdlLogin.GetCredencial(req.body.UserName);    
+   
+  if (credencial.length == 0) {
+    return res.status(403).json({ message: "Usuário não identificado!" });    
+  }  
+
+  if (bCrypt.compareSync(req.body.Password, credencial[0].password)) {
     //auth ok
     const username = credencial[0].username;
     const token = jwt.sign({ username }, process.env.SECRET_API, {
-      expiresIn: 600, // expires in 10min
+      expiresIn: 120*60, //@ Expira em 02 horas
     });
     return res.json({ auth: true, token: token });
   }
 
-  res.status(401).json({ message: "Login inválido!" });
+  res.status(403).json({ message: "Login inválido!" });
 };
 
 function AutenticaJWT(req, res, next) {
   const tokenHeader = req.headers["authorization"];
   if (!tokenHeader)
     return res
-      .status(401)
+      .status(200)
       .json({ auth: false, message: "Não foi informado o token JWT" });
 
   const bearer = tokenHeader.split(" ");
@@ -34,7 +35,7 @@ function AutenticaJWT(req, res, next) {
   jwt.verify(token, process.env.SECRET_API, function (err, decoded) {
     if (err)
       return res
-        .status(401)
+        .status(200)
         .json({ auth: false, message: "JWT inválido ou expirado" });
 
     req.userId = decoded.id;
